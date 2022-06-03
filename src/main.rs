d7351cb4ad57b3d2a7c9a1c2d982ada9cf5914d1
unava1lable <910654119@qq.com>
2022-06-03 19:51:13 +0800

    implement tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", tokenize("(begin (define r 10) (* pi (* r r)))"))
+}
+
+fn tokenize(s: &str) -> Vec<String> {
+    let mut tokens = Vec::new();
+    let mut prev = String::new();
+
+    for c in s.chars() {
+        match c {
+            ' ' => {
+                if !prev.is_empty() {
+                    tokens.push(prev.clone());
+                    prev = String::new();
+                }
+            }
+            c @ ('(' | ')') => {
+                if !prev.is_empty() {
+                    tokens.push(prev.clone());
+                    prev = String::new();
+                }
+                tokens.push(c.to_string());
+            }
+            _ => {
+                prev.push(c);
+            }
+        }
+    }
+    tokens
+}
+
+#[cfg(test)]
+#[test]
+fn test() {
+    assert_eq!(vec!["(".to_string(), ")".to_string()], tokenize("()"));
 }
