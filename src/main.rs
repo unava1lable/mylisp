841f78e9dfe047fddecbc715e6c0e9cdb415d288
unava1lable <910654119@qq.com>
2022-06-03 19:59:28 +0800

    implement another tokenize

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,44 @@ fn main() {
     println!("{:?}", tokenize("(begin (define r 10) (* pi (* r r)))"))
 }
 
-fn tokenize(s: &str) -> Vec<String> {
-    let mut tokens = Vec::new();
-    let mut prev = String::new();
+// fn tokenize(s: &str) -> Vec<String> {
+//     let mut tokens = Vec::new();
+//     let mut prev = String::new();
 
-    for c in s.chars() {
-        match c {
-            ' ' => {
-                if !prev.is_empty() {
-                    tokens.push(prev.clone());
-                    prev = String::new();
-                }
-            }
-            c @ ('(' | ')') => {
-                if !prev.is_empty() {
-                    tokens.push(prev.clone());
-                    prev = String::new();
-                }
-                tokens.push(c.to_string());
-            }
-            _ => {
-                prev.push(c);
-            }
-        }
-    }
-    tokens
+//     for c in s.chars() {
+//         match c {
+//             ' ' => {
+//                 if !prev.is_empty() {
+//                     tokens.push(prev.clone());
+//                     prev = String::new();
+//                 }
+//             }
+//             c @ ('(' | ')') => {
+//                 if !prev.is_empty() {
+//                     tokens.push(prev.clone());
+//                     prev = String::new();
+//                 }
+//                 tokens.push(c.to_string());
+//             }
+//             _ => {
+//                 prev.push(c);
+//             }
+//         }
+//     }
+//     tokens
+// }
+
+fn tokenize(s: &str) -> Vec<String> {
+    s.replace("(", " ( ").replace(")", " ) ").split_whitespace().map(|c| c.to_string()).collect::<Vec<String>>()
 }
 
 #[cfg(test)]
 #[test]
-fn test() {
+fn test1() {
     assert_eq!(vec!["(".to_string(), ")".to_string()], tokenize("()"));
+}
+
+#[test]
+fn test2() {
+    assert_eq!(vec!["(".to_string(), "parse".to_string(), ")".to_string()], tokenize(" (   parse  ) "));
 }
